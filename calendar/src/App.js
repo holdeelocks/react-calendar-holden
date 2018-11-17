e1870adfa350cd8c7e5c7c210635225a396c3772
Holden B <holden.bucher.92@gmail.com>
2018-11-17 15:22:07 -0600

    add events

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      days: {
+        "20181117": {
+          event1: {
+            title: "Study React",
+            time: "8:00",
+            description: "Learn about state and props"
+          },
+          event2: {
+            title: "Learn Redux",
+            time: "9:00",
+            description: "Learn about reducers, action creators and more."
+          }
+        },
+        "20190116": {
+          event1: {
+            title: "Plan a birthday party for Lidiia",
+            time: "00:00",
+            description: "Keep it a secret. Don't reveal the plan"
+          }
+        }
+      }
+    };
+  }
+
+  addEvent = (ev, date) => {
+    let key = Date.now();
+
+    this.setState(prevState => ({
+      days: {
+        ...prevState.days,
+        [date]: {
+          ...prevState.days[date],
+          [key]: ev
+        }
+      }
+    }));
+  };
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App">Planner</div>;
   }
 }
 
