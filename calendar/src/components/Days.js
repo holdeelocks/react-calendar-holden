9a6a1b83827b735d9a24437498f9969c3061a942
Holden B <holden.bucher.92@gmail.com>
2018-11-20 12:25:50 -0600

    more styles

--- calendar/src/components/Days.js
+++ calendar/src/components/Days.js
@@ -1,18 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
-import moment from "../momentRange";
+// import moment from "../momentRange";
+import styled from "styled-components";
+
+const DayWrap = styled.div`
+  height: 100px;
+  width: 100%;
+  display: flex;
+  padding: 1%;
+  border: 1px solid lightslategrey;
+  border-bottom: none;
+  &:last-child {
+    border-bottom: 1px solid lightslategrey;
+  }
+`;
+
+const Event = styled.div`
+  font-weight: bold;
+  margin-left: 2%;
+  display: flex;
+  justify-content: space-between;
+
+  p {
+    margin: 0;
+    margin-left: 2%;
+    border: 1px solid lightblue;
+    text-align: center;
+    &:hover {
+      background-color: lightlblue;
+      color: lightblue;
+      cursor: pointer;
+    }
+  }
+`;
 
 const Day = props => {
   return (
-    <div>
+    <DayWrap>
       {props.date.format("ddd, DD")}
-      <div>
+      <Event>
         {props.events &&
           Object.keys(props.events).map(key => {
-            return props.events[key].title;
+            return (
+              <p key={props.events[key].time}>{props.events[key].title}</p>
+            );
           })}
-      </div>
-    </div>
+      </Event>
+    </DayWrap>
   );
 };
 
